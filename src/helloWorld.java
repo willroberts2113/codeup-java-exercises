90f0e4810e12446b1958bc9b8237c5d301fb0a22
Will Roberts <willroberts2113@gmail.com>
2022-04-06 13:15:17 -0400

    Java Syntax, Data Types, and Variables exercise

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -1,6 +1,41 @@
 class helloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+//        System.out.println("Hello, World!");
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        string class = "class";
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int three = (int) "three";
+
+//        int x = 4;
+//        x = ++x;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
+//        System.out.println(y);
+
+//        short x = 234234232;
+        long y = 234234232;
+        System.out.println(y);
     }
 }
 
